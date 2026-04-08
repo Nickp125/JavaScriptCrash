db59f346897e2650590a97922c019d7e4f707c4b
Nickp125 <npink125@yahoo.com>
2026-04-08 15:38:25 -0400

    Script

---
+++ JavaScript Easy/app.js
@@ -0,0 +1,27 @@
+function addition(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addition(3, 5));
+
+function hoursIntoSeconds(hour) {
+  return hour * 3600;
+}
+
+console.log(hoursIntoSeconds(2));
+
+function calcPerimeter(length, width) {
+  return (length + width) * 2;
+}
+
+console.log(calcPerimeter(6, 7));
+
+function calcAreaOfTriangle(base, width) {
+    return base * width / 2;
+}
+
+console.log(calcAreaOfTriangle(20, 20));
+
+function appendFrontend() {
+    return 
+}
