27410ade00b8becc1e2543e1828aedfa81b2ef14
Nickp125 <npink125@yahoo.com>
2026-04-22 19:23:40 -0400

    Script

--- JavaScript Easy/app.js
+++ JavaScript Easy/app.js
@@ -1,32 +1,57 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
-
-console.log(reverseString("abc"));
-
-function convertToZeros(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = 0;
+function showRating(rating) {
+  let ratings = "";
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratings += "*";
+    if (i !== Math.floor(rating) - 1) {
+      ratings += " ";
+    }
   }
-  return arr;
+
+  if (!Number.isInteger(rating)) {
+    ratings += " .";
+  }
+  return ratings;
 }
 
-console.log(convertToZeros([5, 100, 0]));
+console.log(showRating(3.5));
 
-function removeApples(arr) {
- return arr.filter(elem => elem !== 'Apple')
+function sortLowToHigh(numbers) {
+  return numbers.sort((a, b) => a - b);
 }
 
-console.log(removeApples(["Banana", "Apple", "Orange", "Apple"]));
+console.log(sortHighToLow([1, 5, 0, 10, 4]));
 
-function filterOutFalsy(arr) {
-   return arr.filter(elem => !!elem === true);
+function sortHighToLow(numbers) {
+  return numbers.sort((a, b) => b.price - a.price);
 }
 
-console.log(filterOutFalsy(["", [], 0, null, undefined, "0"]));
+console.log(
+  sortHighToLow([
+    { id: 1, price: 50 },
+    { id: 2, price: 0 },
+    { id: 3, price: 500 },
+  ]),
+);
 
-function convertToBoolean(arr) {
-   return arr.map(elem => !!elem);
+async function postsByUser(userId) {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const result = await promise.json()
+
+  const posts = result.filter(element => element.userId === userId)
+
+  console.log(posts)
 }
 
-console.log(convertToBoolean([500, 0, "David", "", []]));
+postsByUser(4);
+
+async function firstSixIncomplete() {
+
+   const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
+   const result = await promise.json()
+   const posts = result.filter(element => !element.completed).slice(0, 6)
+
+   console.log(posts)
+}
+
+firstSixIncomplete();
